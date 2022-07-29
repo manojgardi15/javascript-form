9a181d9404d5f4718ba2c29e594b502b166c2b06
manojgardi15 <manojgardi157@gmail.com>
2022-07-29 12:46:33 +0530

    added html form and javascript validation

---
+++ index.js
@@ -0,0 +1,62 @@
+//1.way to print in javascript
+// console.log("Hello world");
+// alert("me");
+// document.write("this is document write")
+
+// //2.javascript console API
+
+// console.log("Hello World",4+6, "Another log");
+// console.warn("This is Warning");
+// console.error("this is an error");
+
+//3. JavaScript Variables
+var number1 = 34;
+var number2 = 56;
+//console.log(number1 + number2);
+
+// 4. Data types in javascript
+//number
+
+var num1 = 455;
+var num2 = 56.76;
+
+//string
+var str1 = "This ia a string";
+var str2 = "This is aslo string";
+
+//Object 
+var marks = {
+    ravi: 23,
+    shubham: 65,
+    mj: 99.9
+}
+//console.log(marks);
+
+//Booleans 
+var a = true;
+var b = false;
+//console.log(a, b);
+
+
+var und;
+// console.log(und);
+
+var arr = [1,2,"manoj",4,5]
+//console.log(arr);
+
+//operators in javascript
+
+//Arithmetic operators
+var a=100;
+var b=10;
+console.log("The value of a + b is", a+b);
+console.log("The value of a - b is", a-b);
+console.log("The value of a * b is", a*b);
+console.log("The value of a / b is", a/b);
+
+// Assignment Operators
+var c= b;
+c-=2;
+c*=2;
+c/=2;
+console.log(c);
