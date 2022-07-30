586794bba7a246bee48fd2f42fcb73996735e621
manojgardi15 <manojgardi157@gmail.com>
2022-07-30 12:18:33 +0530

    added validation in the form

---
+++ validation.js
@@ -0,0 +1,31 @@
+function validation() {
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+    
+    if (firstname == "") {
+        document.getElementById("fname").innerHTML =
+            "Please fill the First Name field";
+        return false;
+    }
+
+    if (lastname == "") {
+        document.getElementById("lname").innerHTML =
+            "Please fill the Last Name field";
+        return false;
+    }
+
+    if (email == "") {
+        document.getElementById("mail").innerHTML =
+            "Please fill the Email field";
+        return false;
+    }
+
+    if (mobile == "") {
+        document.getElementById("mobileno").innerHTML =
+            "Please fill the Mobile Number field";
+        return false;
+    }
+
+}
