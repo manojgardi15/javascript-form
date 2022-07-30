1a182dc7b862e41982ad0265dc298b1b5b3018e7
manojgardi15 <manojgardi157@gmail.com>
2022-07-30 23:23:30 +0530

    javascript validation is applied to the checkbox field

--- validation.js
+++ validation.js
@@ -3,29 +3,39 @@ function validation() {
     const lastname = document.getElementById('lastname').value;
     const email = document.getElementById('email').value;
     const mobile = document.getElementById('mobile').value;
-    
+    const accept = document.getElementsByName('checkbox').checked;
+
     if (firstname == "") {
         document.getElementById("fname").innerHTML =
-            "Please fill the First Name field";
+            "*Please fill the First Name field";
         return false;
     }
 
     if (lastname == "") {
         document.getElementById("lname").innerHTML =
-            "Please fill the Last Name field";
+            "*Please fill the Last Name field";
         return false;
     }
 
     if (email == "") {
         document.getElementById("mail").innerHTML =
-            "Please fill the Email field";
+            "*Please fill the Email field";
         return false;
     }
 
     if (mobile == "") {
         document.getElementById("mobileno").innerHTML =
-            "Please fill the Mobile Number field";
+            "*Please fill the Mobile Number field";
         return false;
     }
 
+    if (document.getElementById('accept').checked)
+     { 
+        return true;
+     } 
+     else {
+        document.getElementById("condition").innerHTML =
+        "*Checkbox not checked";
+          return false; 
+        }
 }
