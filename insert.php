c3f51c05169ef6a8d02acacd11254161a18fad19
manojgardi15 <109799195+manojgardi15@users.noreply.github.com>
2022-09-18 23:35:20 +0530

    add variables

--- insert.php
+++ insert.php
@@ -5,7 +5,10 @@ if(isset($_POST['submit']))
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $gender = $_POST['gender'];
-//$birth_date = $_POST['birth_date'];
+$birth_date = date('y-m-d', strtotime($_POST['birth_date']));
+$aadharcard_no = $_POST['aadharcard_no'];
+$joining_date = date('y-m-d', strtotime($_POST['joining_date']));
+$designation = $_POST['designation'];
 $email = $_POST['email'];
 $mobile_number = $_POST['mobile_number'];
 $address = $_POST['address'];
@@ -14,8 +17,8 @@ $state = $_POST['state'];
 
 // database connection
 
-$sql = "INSERT INTO newemployee (first_name,last_name,gender,email,mobile_number,address,city,state)
-     VALUES ('$first_name','$last_name','$gender','$email','$mobile_number','$address','$city','$state')";
+$sql = "INSERT INTO newemployee (first_name,last_name,gender,birth_date,aadharcard_no,joining_date,designation,email,mobile_number,address,city,state)
+     VALUES ('$first_name','$last_name','$gender','$birth_date','$aadharcard_no','$joining_date','$designation','$email','$mobile_number','$address','$city','$state')";
      if (mysqli_query($conn, $sql)) {
         echo "New record has been added successfully !";
      } else {
